5854687ef61f7ecf7cb534fd8251658d1c46c344
xiaochenxiaochen123 <chenhao067402@163.com>
2024-10-22 23:05:39 +0800

    featrue(limits): 各个数据类型的长度

---
+++ src/limits.cpp
@@ -0,0 +1,31 @@
+//limits.cpp --some integer limits
+#include <iostream>
+#include <climits>
+
+static int limits()
+{
+	using namespace std;
+	int n_int = INT_MAX;
+	short n_short = SHRT_MAX;
+	long n_long = LONG_MAX;
+	long long n_llong = LLONG_MAX;
+
+	// sizeof operator yields size of type or of variable 
+	cout << "int is " << sizeof(int) << "bytes." << endl;
+	cout << "short is " << sizeof(n_short) << "bytes." << endl;
+	cout << "long is " << sizeof(n_long) << "bytes." << endl;
+	cout << "long long is " << sizeof(n_llong) << "bytes." << endl;
+	cout << endl;
+
+	cout << "MAX values:" << endl;
+	cout << "int : " << n_int << endl;
+	cout << "short : " << n_short << endl;
+	cout << "long : " << n_long << endl;
+	cout << "long long : " << n_llong << endl;
+
+	cout << "MIN int value : " << INT_MIN << endl;
+	cout << "Bits per byte : " << CHAR_BIT << endl;
+	cout << CHAR_MIN << endl;
+	cout << CHAR_MAX << endl;
+	return 0;
+}
